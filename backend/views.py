588da961f2dcbac7529f6194fa7ecbac388eba87
zhangyue <2698051674@qq.com>
2019-07-12 16:59:34 +0800

    搭建django+vue+elementui项目

---
+++ backend/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import JsonResponse
+
+def index(request):
+    data = [
+        {
+            "date": '2016-05-02',
+            "name": '王小虎',
+            "sex": '男'
+        },
+        {
+            "date": '2016-05-02',
+            "name": '王小虎',
+            "sex": '男'
+        },
+        {
+            "date": '2016-05-02',
+            "name": '王小虎',
+            "sex": '男'
+        },
+        {
+            "date": '2016-05-02',
+            "name": '王小虎',
+            "sex": '男'
+        },
+    ]
+
+    return JsonResponse(data, safe=False)
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <el-table
+    :data="tableData"
+  >
+      <el-table-column
+        prop="date"
+        label="日期"
+      ></el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="sex"
+        label="性别">
+      </el-table-column>
+    </el-table>
+  </div>
+
+</template>
+
+<script>
+  import axios from 'axios'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      tableData: []
+    }
+  },
+  created () {
+    axios.get('http://127.0.0.1:8000/backend/').then(response => {
+      this.tableData = response.data;
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
