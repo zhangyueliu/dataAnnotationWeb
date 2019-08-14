d301b7ca3ed7c99aa67038805d258fe09aa74ec1
zhangyue <2698051674@qq.com>
2019-08-14 19:54:24 +0800

    搭建界面

---
+++ frontend/src/components/MainView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="img-view">
+      <div id="img_box"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MainView"
+    }
+</script>
+
+<style scoped>
+  .img-view {
+    padding: 10px;
+  }
+  #img_box {
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+  }
+  #img_box >>> img{
+    visibility: hidden;
+  }
+</style>
